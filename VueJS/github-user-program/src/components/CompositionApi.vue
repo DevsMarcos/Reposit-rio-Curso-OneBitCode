fbde76f5017af623ebaa29f6bbb6764ab539b7c6
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-02-13 17:16:42 -0300

    Atualização das aulas

--- VueJS/github-user-program/src/components/CompositionApi.vue
+++ VueJS/github-user-program/src/components/CompositionApi.vue
@@ -1,17 +1,20 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import { computed } from '@vue/reactivity';
+
+const searchInput = ref('')
 
 const state = reactive({
-  login: 'johndoe',
-  name: 'John Doe',
-  bio: '...',
-  company: 'Acme Inc.',
-  avatar_url: 'https://unsplash.it/256',
-  searchInput: ''
+  login: '',
+  name: '',
+  bio: '',
+  company: '',
+  avatar_url: '',
+  repos:  [],
 })
 
 async function fetchGithubUser() {
-  const res = await fetch(`https://api.github.com/users/${state.searchInput}`)
+  const res = await fetch(`https://api.github.com/users/${searchInput.value}`)
   const { login, name, bio, company, avatar_url } = await res.json()
 
   state.login = login
@@ -19,18 +22,49 @@ async function fetchGithubUser() {
   state.bio = bio
   state.company = company
   state.avatar_url = avatar_url
+  state.repos = []
+
+  fetchUserRepositores(login)
+
 }
+
+async function fetchUserRepositores(username) {
+  const res = await fetch(`https://api.github.com/users/${username}/repos`)
+  const repos = await res.json()
+
+  state.repos = repos
+
+
+}
+
+const reposCountMessage = computed(() => {
+  return state.repos.length > 0
+    ? `${state.name} possui ${state.repos.length} repositórios públicos.`
+    : `${state.name} não possui nenhum repositório público.`
+})
+
 </script>
 
 <template>
-	<h1>GitHub User Data</h1>
-  <input type="text" v-model="state.searchInput">
+  <h1>GitHub User Data</h1>
+  <input type="text" v-model="searchInput">
   <button v-on:click="fetchGithubUser">Carregar Usuário</button>
-  <img v-bind:src="state.avatar_url">
-  <strong>@{{ state.login }}</strong>
-  <h2>{{ state.name }}</h2>
-  <h3>{{ state.company }}</h3>
-  <span>{{ state.bio }}</span>
+  <div v-if="state.login">
+    <img v-bind:src="state.avatar_url">
+    <strong>@{{ state.login }}</strong>
+    <h2>{{ state.name }}</h2>
+    <h3>{{ state.company }}</h3>
+    <span>{{ state.bio }}</span>
+
+    <section v-if="state.repos.length > 0">
+      <h2>{{ reposCountMessage }}</h2>
+      <article v-for="repo of state.repos">
+        <h3>{{ repo.full_name }}</h3>
+        <p>{{ repo.description }}</p>
+        <a :href="repo.html_url" target="_blank">Ver no GitHub</a>
+      </article>
+    </section>
+  </div>
 </template>
 
 <style scoped>
